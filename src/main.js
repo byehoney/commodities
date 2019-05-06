900094f7d4bbbd8f852317cc54a2d5642dd4dbe3
杨海强 <haiqiang.yang@chetuobang.com>
2019-05-06 13:41:10 +0800

    main 冲突

--- src/main.js
+++ src/main.js
@@ -4,15 +4,12 @@ import router from './router'
 import store from './store/index'
 import filters from './filters/index'
 import wechat from '@/common/wechat/index'
-<<<<<<< HEAD
 import DrawerLayout from 'vue-drawer-layout'
 Vue.use(DrawerLayout)
-=======
 import Mint from "mint-ui"
 import { Tabbar, TabItem } from 'mint-ui';
 Vue.component(Tabbar.name, Tabbar);
 Vue.component(TabItem.name, TabItem);
->>>>>>> e8dcad535467f8be790ed193e6cb46eaa96a8c48
 Vue.use(wechat);
 // 注入全局过滤器
 Object.keys(filters).forEach(item => {
