df3ad8f8c7844ed168a7444e1a8bbd5b76a34f73
杨海强 <haiqiang.yang@chetuobang.com>
2019-05-08 11:53:26 +0800

    搜索记录功能

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="searchBox">
             <input v-model="searchStr" class="searchInput"  placeholder="请输入搜索内容"/>
-            <div class="searchBtn" @click="goSearch">搜索</div>
+            <div class="searchBtn" @click="SearchVal">搜索</div>
         </div>
         <ul>
-            <li v-for="(item,index) in searchArr" :key="index">
+            <li v-for="(item,index) in HistoryList" :key="index">
                 {{item}}  <span @click="delSearch(index)">X</span>
             </li>
         </ul>
@@ -16,49 +16,37 @@ export default {
     data(){
         return{
             searchStr:'',
-            searchArr:[],
+            HistoryList:[],
         }
     },
     created(){
-        let history = localStorage.getItem('searchHistory')?localStorage.getItem('searchHistory').split(','):[];
-        history = this.uniq(history);
-        this.searchArr = history;
+        let history = localStorage.getItem('HistoryList')?JSON.parse(localStorage.getItem('HistoryList').split(',')):[];
+        this.HistoryList = history;
     },
     components:{
         
     },
     methods:{
-        goSearch(){
-            let history = localStorage.getItem('searchHistory')?localStorage.getItem('searchHistory'):'';
-            if(this.searchStr!=''){
-                console.log(history)
-                if(history){
-                    history+=','+this.searchStr;
-                }else{
-                    history=this.searchStr;
+        SearchVal () {
+            let val = this.searchStr.trim() // 清除空格
+            if (this.HistoryList.length > 0) { // 有数据的话 判断
+                if (this.HistoryList.indexOf(val) !== -1) { // 有相同的，先删除 再添加 
+                    this.HistoryList.splice(this.HistoryList.indexOf(val), 1)
+                    this.HistoryList.unshift(val)
+                } else { // 没有相同的 添加
+                    this.HistoryList.unshift(val)
                 }
-                                
-                history = history.split(',');
-                let rec = history;
-                this.searchArr = rec.reverse();
-                localStorage.setItem('searchHistory',history.join(','))
+            } else { // 没有数据 添加
+                this.HistoryList.unshift(val)
             }
+            if (this.HistoryList.length > 6) { // 保留六个值
+                this.HistoryList.pop()
+            }
+            localStorage.setItem('HistoryList', JSON.stringify(this.HistoryList))
         },
         delSearch(index){
-            let history = localStorage.getItem('searchHistory').split(',');
-            history.splice(index,1);
-            console.log(history)
-            localStorage.setItem('searchHistory',history.join(','))
-            this.searchArr = history;
-        },
-        uniq(array){
-            var temp = []; //一个新的临时数组
-            for(var i = 0; i < array.length; i++){
-                if(temp.indexOf(array[i]) == -1){
-                    temp.push(array[i]);
-                }
-            }
-            return temp;
+            this.HistoryList.splice(index,1);
+            localStorage.setItem('HistoryList', JSON.stringify(this.HistoryList))
         }
     }
 }
