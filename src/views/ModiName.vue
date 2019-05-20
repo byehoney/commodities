c559a73f1fae0c072c27d8afcf3586fae5dca203
杨海强 <haiqiang.yang@chetuobang.com>
2019-05-20 14:03:27 +0800

    提交修改姓名 修改手机号

--- src/views/ModiName.vue
+++ src/views/ModiName.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        修改姓名
+    <div class="nameContainer">
+        <div class="nav">
+            <img @click="goBack" src="../images/leftArrow.png" class="leftIcon" alt="">
+            <span class="title">修改姓名</span>
+            <span class="btn" @click="saveName">保存</span>
+        </div>
+        <input class="input" type="text" autocomplete="false" v-model="name" placeholder="请输入姓名">
     </div>
 </template>
 <script>
@@ -9,6 +14,71 @@ export default {
         return{
             
         }
+    },
+    methods:{
+        goBack(){
+            this.$router.go(-1);
+        },
+        saveName(){
+
+        }
     }
 }
 </script>
+<style lang="scss" scoped>
+.nameContainer{
+    width: 100vw;
+    height: 100vh;
+    background:rgba(235,235,235,1);
+    .nav{
+        width: 100%;
+        height: 88px;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fff;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1000;
+        border-bottom: 2px solid #ebebeb;
+        .leftIcon{
+            width: 17px;
+            height: 30px;
+            position: absolute;
+            top: 29px;
+            left: 30px;
+        }
+        .title{
+            font-size: 30px;
+        }
+        .btn{
+            width:116px;
+            height:35px;
+            font-size:26px;
+            color:rgba(255,0,0,1);
+            line-height:35px;
+            letter-spacing:3px;
+            position: absolute;
+            top: 26px;
+            right: 41px;
+            text-align: right;
+        }
+    }
+    .input{
+        width: 100%;
+        height: 120px;
+        line-height: 120px;
+        background-color: #fff;
+        border: none;
+        outline: none;
+        padding-top: 92px;
+        display: block;
+        font-size:26px;
+        color:#333;
+        letter-spacing:1px;
+        padding-left: 39px;
+    }
+}
+</style>
--- src/views/ModiTel.vue
+++ src/views/ModiTel.vue
@@ -1,14 +1,105 @@
 <template>
-    <div>
-        修改手机号
+    <div class="telContainer">
+        <TopNav></TopNav>
+        <div class="telContent">
+            <div class="tip">您正在更换手机号：139****8888</div>
+            <div class="text">请验证</div>
+            <div class="sendArea">
+                <input v-model="yzm" type="text" autocomplete="false" placeholder="请输入验证码" class="left">
+                <div class="right" @click="sendMsg">{{msgStr}}</div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import { Toast } from "mint-ui";
+import TopNav from '@/components/TopNav'
 export default {
     data(){
         return{
-            
+            yzm:'',
+            sending:false,
+            msgStr:"获取验证码",
+        }
+    },
+    components:{
+        TopNav,
+    },
+    methods:{
+        sendMsg(){
+            if(!this.sending){
+                let timer = null;
+                let step = 60;
+                this.sending = true;
+                timer = setInterval(()=>{
+                    step--;
+                    if(step==0){
+                        this.msgStr = '重新发送' ;
+                        clearInterval(timer);
+                        this.sending = false;
+                    }else{
+                        this.msgStr = step+'s后重新发送' ;
+                    }
+                },1000) 
+            }
         }
     }
 }
 </script>
+<style lang="scss" scoped>
+.telContainer{
+    width: 100vw;
+    height: 100vh;
+    background:rgba(235,235,235,1);
+    .telContent{
+        padding-top: 120px;
+        display: flex;
+        flex-direction: column;
+        padding-left: 39px;
+        padding-right: 41px;
+        .tip{
+            font-size:28px;
+            color:rgba(51,51,51,1);
+            line-height:37px;
+            letter-spacing:2px;
+            margin-bottom: 18px;
+        }
+        .text{
+            font-size:26px;
+            color:rgba(102,102,102,1);
+            line-height:35px;
+            letter-spacing:1px;
+            margin-bottom: 38px;
+        }
+        .sendArea{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            .left{
+                width: 331px;
+                height: 96px;
+                line-height: 96px;
+                border: 2px solid #ebebeb;
+                background-color: #fff;
+                font-size:28px;
+                color:rgba(235,235,235,1);
+                letter-spacing:1px;
+                padding-left: 17px;
+                margin-right: 16px;
+                border-radius: 10px;
+            }
+            .right{
+                width: 315px;
+                height: 94px;
+                border: 2px solid #FF001F;
+                font-size:28px;
+                color:rgba(255,0,0,1);
+                line-height:94px;
+                letter-spacing:2px;
+                border-radius: 10px;
+                text-align: center;
+            }
+        }
+    }
+}
+</style>
