d682e20c34f11d6b4b63e3b6e0531b9e5366a98a
yhq <2905922480@qq.com>
2019-06-02 18:29:54 +0800

    修改分类

--- src/views/Classify.vue
+++ src/views/Classify.vue
@@ -21,11 +21,13 @@
                 <span>{{item.title}}</span>
               </div>
             </div>
+
+            <div class="class_content_right " :class="isActive==index?active_bg:''">
+              <div :class='isActive==index?is_active:""'></div>
+              <span v-for="(data,index) in item.data" :key="index">{{data.name}}</span>
+            </div>
           </li>
         </ul>
-        <div class="class_content_right " :class="isActive==index?active_bg:''">
-          
-        </div>
       </div>
     </div>
   </div>
