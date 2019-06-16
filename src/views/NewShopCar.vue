4962bda0d7ee11130d86112f79186a2acfe75520
yhq <2905922480@qq.com>
2019-06-16 22:09:37 +0800

    提交

--- src/views/NewShopCar.vue
+++ src/views/NewShopCar.vue
@@ -119,142 +119,48 @@
   </div>
 </template>
 <script>
+import { Toast } from "mint-ui";
+import { mapGetters,mapState } from 'vuex'
+import { getCarList } from '@/api/index'
 import TopNav from "@/components/TopNav";
 export default {
   data() {
     return {
       result: true, //购物车编辑态
-      fetchData: {
-        list: [
-          {
-            shop_id: 1,
-            shop_name: "万家灯火烟花有限公司1",
-            pro_brand: "每日采集额：999",
-            products: [
-              {
-                pro_id: 101,
-                pro_text: "全家福 冷焰火烟花1",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              },
-              {
-                pro_id: 102,
-                pro_text: "全家福 冷焰火烟花2",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              },
-              {
-                pro_id: 103,
-                pro_text: "全家福 冷焰火烟花3",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              }
-            ],
-            check: false, //店铺选中状态
-            choose: 0 //商品选中个数
-          },
-          {
-            shop_id: 2,
-            shop_name: "卷卷旗舰店2",
-            pro_brand: "每日采集额：999",
-            products: [
-              {
-                pro_id: 201,
-                pro_text: "全家福 冷焰火烟花21",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              },
-              {
-                pro_id: 202,
-                pro_text: "全家福 冷焰火烟花22",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              }
-            ],
-            check: false,
-            choose: 0
-          },
-          {
-            shop_id: 3,
-            shop_name: "瓜皮的神秘商店3",
-            pro_brand: "每日采集额：999",
-            products: [
-              {
-                pro_id: 301,
-                pro_text: "全家福 冷焰火烟花31",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              },
-              {
-                pro_id: 302,
-                pro_text: "全家福 冷焰火烟花32",
-                pro_price: 480,
-                pro_num: 1,
-                pro_img: "./images/1.png",
-                pro_sum: 480,
-                pro_place: "河北保定星星烟花制造厂",
-                pro_depot: "规格：35g*1支",
-                pro_purity: "选择赠品>",
-                checked: false //商品选中状态
-              }
-            ],
-            check: false,
-            choose: 0
-          }
-        ],
-        status: false, //全选选中状态
-        allchoose: 0, //店铺选中个数
-        allsum: 0, //总计价格
-        allnum: 0, //总计数量
-        is_success: true //控制管理和完成的显示
-      }
+      head:{},//公共头
+      list:[],//普通列表
+      mzList:[],//买赠列表
+
     };
+  },
+  computed:{
+    ...mapGetters('login',['token','userId','corpCode','companyId','userRole']),
+    ...mapState('login',['user']),
   },
   methods: {
     complete() {
       this.result = !this.result;
+    },
+    async getData(){
+      let defaulParams = {
+        token:this.token,
+        userId:this.userId,
+        corpCode:this.corpCode,
+        companyId:this.companyId,
+        userRole:this.userRole,
+      };
+      let res = await getCarList(defaulParams);
+      this.head = res.data.head;
+      this.list = res.data.list;
+      this.mzList = res.data.mzList;
     }
   },
   components: {
     TopNav
   },
-  mounted() {}
+  mounted() {
+    this.getData();
+  }
 };
 </script>
 <style lang="scss" scoped>
