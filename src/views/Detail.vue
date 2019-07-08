83016e42c716144e0b8fe7473d9b5644d5e7aa65
杨海强 <haiqiang.yang@chetuobang.com>
2019-07-08 18:30:14 +0800

    视频

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -7,7 +7,7 @@
     <div class="detail_loop">
       <div class="detsil_loop_center">
         <mt-swipe :auto="0" :show-indicators="true">
-          <!-- <mt-swipe-item class="item">
+          <mt-swipe-item class="item">
             <video
               class="myVideo"
               playsinline="true"
@@ -15,20 +15,22 @@
               x-webkit-airplay="allow"
               webkit-playsinline="true" 
               x5-video-player-type="h5"
+              x5-video-player-fullscreen="true"
+              x5-video-orientation="landscape"
               :poster="shopDetail.yt"
               controls=""
             >
               <source src="http://qxindian.fireworks-online.cn/15%E6%B3%A2%E9%87%91%E4%B8%9D%E6%9F%B3%E5%90%90%E7%8F%A0-.mp4">
             </video>
-          </mt-swipe-item> -->
-          <mt-swipe-item class="item">
+          </mt-swipe-item>
+          <!-- <mt-swipe-item class="item">
             <video
               class="myVideo"
               controls=""
             >
               <source src="http://qxindian.fireworks-online.cn/15%E6%B3%A2%E9%87%91%E4%B8%9D%E6%9F%B3%E5%90%90%E7%8F%A0-.mp4">
             </video>
-          </mt-swipe-item>
+          </mt-swipe-item> -->
           <mt-swipe-item class="item">
             <img :src="shopDetail.yt?shopDetail.yt:require('../images/default_logo.jpg')">
           </mt-swipe-item>
